37e8c22ebe82e1b130c000361804a34f2269323b
Federico Diaz <soyfedediazz@gmail.com>
2026-08-20 20:56:04 -0300

    Practico2
    Resolucion del practico 2

---
+++ Pequeño Formulario/Practico2/Practico3/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace Practico3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LNyA = new System.Windows.Forms.Label();
+            this.LDni = new System.Windows.Forms.Label();
+            this.LApellido = new System.Windows.Forms.Label();
+            this.LNombre = new System.Windows.Forms.Label();
+            this.LModificar = new System.Windows.Forms.Label();
+            this.TDni = new System.Windows.Forms.TextBox();
+            this.TApellido = new System.Windows.Forms.TextBox();
+            this.TNombre = new System.Windows.Forms.TextBox();
+            this.TGuardar = new System.Windows.Forms.Button();
+            this.TEliminar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // LNyA
+            // 
+            this.LNyA.AutoSize = true;
+            this.LNyA.Location = new System.Drawing.Point(68, 26);
+            this.LNyA.Name = "LNyA";
+            this.LNyA.Size = new System.Drawing.Size(95, 13);
+            this.LNyA.TabIndex = 0;
+            this.LNyA.Text = "Nombre y Apellido:";
+            // 
+            // LDni
+            // 
+            this.LDni.AutoSize = true;
+            this.LDni.Location = new System.Drawing.Point(68, 59);
+            this.LDni.Name = "LDni";
+            this.LDni.Size = new System.Drawing.Size(26, 13);
+            this.LDni.TabIndex = 1;
+            this.LDni.Text = "DNI";
+            // 
+            // LApellido
+            // 
+            this.LApellido.AutoSize = true;
+            this.LApellido.Location = new System.Drawing.Point(68, 100);
+            this.LApellido.Name = "LApellido";
+            this.LApellido.Size = new System.Drawing.Size(44, 13);
+            this.LApellido.TabIndex = 2;
+            this.LApellido.Text = "Apellido";
+            // 
+            // LNombre
+            // 
+            this.LNombre.AutoSize = true;
+            this.LNombre.Location = new System.Drawing.Point(68, 137);
+            this.LNombre.Name = "LNombre";
+            this.LNombre.Size = new System.Drawing.Size(44, 13);
+            this.LNombre.TabIndex = 3;
+            this.LNombre.Text = "Nombre";
+            // 
+            // LModificar
+            // 
+            this.LModificar.AutoSize = true;
+            this.LModificar.ForeColor = System.Drawing.Color.Red;
+            this.LModificar.Location = new System.Drawing.Point(268, 26);
+            this.LModificar.Name = "LModificar";
+            this.LModificar.Size = new System.Drawing.Size(49, 13);
+            this.LModificar.TabIndex = 4;
+            this.LModificar.Text = "modificar";
+            // 
+            // TDni
+            // 
+            this.TDni.Location = new System.Drawing.Point(217, 56);
+            this.TDni.Name = "TDni";
+            this.TDni.Size = new System.Drawing.Size(100, 20);
+            this.TDni.TabIndex = 5;
+            this.TDni.TextChanged += new System.EventHandler(this.TDni_TextChanged);
+            this.TDni.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TDni_KeyPress);
+            // 
+            // TApellido
+            // 
+            this.TApellido.Location = new System.Drawing.Point(217, 93);
+            this.TApellido.Name = "TApellido";
+            this.TApellido.Size = new System.Drawing.Size(100, 20);
+            this.TApellido.TabIndex = 6;
+            this.TApellido.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TApellido_KeyPress);
+            // 
+            // TNombre
+            // 
+            this.TNombre.Location = new System.Drawing.Point(217, 130);
+            this.TNombre.Name = "TNombre";
+            this.TNombre.Size = new System.Drawing.Size(100, 20);
+            this.TNombre.TabIndex = 7;
+            this.TNombre.TextChanged += new System.EventHandler(this.TNombre_TextChanged);
+            this.TNombre.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TNombre_KeyPress);
+            // 
+            // TGuardar
+            // 
+            this.TGuardar.Location = new System.Drawing.Point(88, 192);
+            this.TGuardar.Name = "TGuardar";
+            this.TGuardar.Size = new System.Drawing.Size(75, 23);
+            this.TGuardar.TabIndex = 8;
+            this.TGuardar.Text = "Guardar";
+            this.TGuardar.UseVisualStyleBackColor = true;
+            this.TGuardar.Click += new System.EventHandler(this.TGuardar_Click);
+            // 
+            // TEliminar
+            // 
+            this.TEliminar.Location = new System.Drawing.Point(194, 192);
+            this.TEliminar.Name = "TEliminar";
+            this.TEliminar.Size = new System.Drawing.Size(75, 23);
+            this.TEliminar.TabIndex = 9;
+            this.TEliminar.Text = "Eliminar";
+            this.TEliminar.UseVisualStyleBackColor = true;
+            this.TEliminar.Click += new System.EventHandler(this.TEliminar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 311);
+            this.Controls.Add(this.TEliminar);
+            this.Controls.Add(this.TGuardar);
+            this.Controls.Add(this.TNombre);
+            this.Controls.Add(this.TApellido);
+            this.Controls.Add(this.TDni);
+            this.Controls.Add(this.LModificar);
+            this.Controls.Add(this.LNombre);
+            this.Controls.Add(this.LApellido);
+            this.Controls.Add(this.LDni);
+            this.Controls.Add(this.LNyA);
+            this.Name = "Form1";
+            this.Text = "Pequeño Formulario";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label LNyA;
+        private System.Windows.Forms.Label LDni;
+        private System.Windows.Forms.Label LApellido;
+        private System.Windows.Forms.Label LNombre;
+        private System.Windows.Forms.Label LModificar;
+        private System.Windows.Forms.TextBox TDni;
+        private System.Windows.Forms.TextBox TApellido;
+        private System.Windows.Forms.TextBox TNombre;
+        private System.Windows.Forms.Button TGuardar;
+        private System.Windows.Forms.Button TEliminar;
+    }
+}
+
---
+++ Pequeño Formulario/Practico2/Practico3/Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Practico3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void TDni_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TDni_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // permite solo digitos numericos
+            if (!char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar))
+            {
+                e.Handled = true; // Bloquea cualquier otra tecla
+            }
+        }
+
+        private void TApellido_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // Permite letras, espacios en blanco y teclas de control (como Retroceso / Backspace)
+            if (!char.IsLetter(e.KeyChar) && !char.IsControl(e.KeyChar) && !char.IsWhiteSpace(e.KeyChar))
+            {
+                e.Handled = true; // Bloquea números o caracteres especiales
+            }
+        }
+
+        private void TNombre_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TNombre_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // Permite letras, espacios en blanco y teclas de control (como Retroceso / Backspace)
+            if (!char.IsLetter(e.KeyChar) && !char.IsControl(e.KeyChar) && !char.IsWhiteSpace(e.KeyChar))
+            {
+                e.Handled = true; // Bloquea números o caracteres especiales
+            }
+        }
+
+        private void TGuardar_Click(object sender, EventArgs e)
+        {
+            // 1. Verificación de campos incompletos
+            if (TDni.Text.Trim() == "" || TApellido.Text.Trim() == "" || TNombre.Text.Trim() == "")
+            {
+                DialogResult ask;
+                ask = MessageBox.Show("Debe Completar todos los campos", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                // 2. Pregunta de confirmación al usuario
+                DialogResult ask;
+
+                ask = MessageBox.Show(
+                    "Seguro que desea insertar un nuevo Cliente?",
+                    "Confirmar Insercion",
+                    MessageBoxButtons.YesNo,
+                    MessageBoxIcon.Question,
+                    MessageBoxDefaultButton.Button1
+                );
+
+                // 3. Si confirma haciendo clic en "SÍ"
+                if (ask == DialogResult.Yes)
+                {
+                    // Concatenación de los campos para la acción solicitada
+                    LModificar.Text = $"{TNombre.Text} {TApellido.Text}";
+                    //LModificar.Text = "Modificado";
+
+                    // 4. MsgBox de Información con el nombre ingresado
+                    MessageBox.Show(
+                        $"El Cliente: {TNombre.Text} {TApellido.Text} se insertó correctamente",
+                        "Guardar",
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Information
+                    );
+
+                }
+
+            }
+
+        }
+
+        private void TEliminar_Click(object sender, EventArgs e)
+        {
+            // Guardamos el nombre completo en una variable antes de limpiar los campos
+            string cliente = $"{TNombre.Text} {TApellido.Text}";
+
+            // Variable para capturar la respuesta
+            DialogResult ask;
+
+            // MsgBox de Exclamación con el focus en "NO" (Button2)
+            ask = MessageBox.Show(
+                $"Está apunto de eliminar el Cliente: {cliente}",
+                "Confirmar Eliminación",
+                MessageBoxButtons.YesNo,
+                MessageBoxIcon.Exclamation,
+                MessageBoxDefaultButton.Button2
+            );
+
+            // Evaluación: si presiona "SÍ" ejecuta la eliminación
+            if (ask == DialogResult.Yes)
+            {
+                // 1. Muestra el mensaje de confirmación
+                MessageBox.Show(
+                    $"El Cliente: {cliente} se eliminó correctamente",
+                    "Eliminar",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Information
+                );
+
+                // 2. Limpia todos los TextBox
+                TDni.Clear();
+                TApellido.Clear();
+                TNombre.Clear();
+                //txtResultado.Clear(); // Reemplazar por el Name real de tu TextBox multilínea
+
+                // 3. Limpia el Label Lmodificar
+                LModificar.Text = "";
+            }
+            // Si presiona "NO", no ejecuta nada y el código finaliza sin hacer cambios
+        }
+    }
+
+}
